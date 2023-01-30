5c15ab3c9fbbc5110dd50840ebccde65929314e0
DecarieDavies <decarie.davies@uea.ac.uk>
2023-01-30 17:31:51 +0000

    made script with no errors

---
+++ intro stats.R
@@ -0,0 +1,152 @@
+library(tidyverse)
+library(here)
+# put this at top of script
+# put this at top of script
+library(kableExtra)
+
+# use kable extra functions to make a nice table (could be replaced with kable() if needed)
+darwin_summary %>% 
+  kbl(caption="Summary statistics of crossed and selfed maize plants") %>% 
+  kable_styling(bootstrap_options = "striped", full_width = T, position = "left")
+darwin <- read_csv(here("data", "darwin.csv"))
+
+# check the structure of the data
+glimpse(darwin)
+
+# check data is in a tidy format
+head(darwin)
+
+# check variable names
+colnames(darwin)
+
+
+# clean up column names
+
+darwin <- janitor::clean_names(darwin)
+# exploting data visualisation  to portray what the data holds
+darwin %>% 
+  ggplot(aes(x=type,
+             y=height))+
+  geom_point()
+
+# distributing the tidy functions and standad deviations of the group 
+darwin %>% 
+  group_by(type) %>% 
+  summarise(mean=mean(height),
+            sd=sd(height))
+
+# make a new object
+darwin_summary <-darwin %>% 
+  group_by(type) %>% 
+  summarise(mean=mean(height),
+            sd=sd(height))
+
+# make a summary plot
+darwin_summary %>% 
+  ggplot(aes(x=type,
+             y=mean))+
+  geom_pointrange(aes(ymin=mean-sd, ymax=mean+sd))+
+  theme_bw()
+
+# pivot data to wide format then subtract Selfed plant heights from Crossed plant heights allowing new colums to be creatd and labled 
+
+darwin_wide <- darwin %>% 
+  pivot_wider(names_from = type, values_from = height) %>% 
+  mutate(difference = Cross - Self)
+
+difference_summary <- darwin_wide %>% 
+  summarise(mean=mean(difference),
+            sd=sd(difference),
+            n=n())
+
+difference_summary
+
+# understanding tlevel of standard rror ill allow us to determin how rliable it is 
+difference_summary %>% 
+  mutate(se= sd/sqrt(n))
+
+
+# check for duplication
+darwin %>% 
+  duplicated() %>% 
+  sum()
+
+# check for typos - by looking at impossible values
+darwin %>% 
+  summarise(min=min(height, na.rm=TRUE), 
+            max=max(height, na.rm=TRUE))
+
+# check for typos by looking at distinct characters/values
+# clean and check the dtaa for anything that will inhibit te code for running 
+
+darwin %>% 
+  distinct(pair)
+
+darwin %>% 
+  distinct(type)
+
+# missing values
+darwin %>% 
+  is.na() %>% 
+  sum()
+
+# quick summary
+
+summary(darwin)
+
+#seperation of standard deviation in order to understand what the mean is 
+
+darwin %>% 
+  group_by(type) %>% 
+  summarise(mean=mean(height),
+            sd=sd(height))
+
+# make a new object
+darwin_summary <-darwin %>% 
+  group_by(type) %>% 
+  summarise(mean=mean(height),
+            sd=sd(height))
+
+# make a summary plot
+darwin_summary %>% 
+  ggplot(aes(x=type,
+             y=mean))+
+  geom_pointrange(aes(ymin=mean-sd, ymax=mean+sd))+
+  theme_bw()
+
+# put this at top of script
+library(kableExtra)
+
+# use kable extra functions to make a nice table (could be replaced with kable() if needed)
+darwin_summary %>% 
+  kbl(caption="Summary statistics of crossed and selfed maize plants") %>% 
+  kable_styling(bootstrap_options = "striped", full_width = T, position = "left")
+# pivot data to wide format then subtract Selfed plant heights from Crossed plant heights
+
+darwin_wide <- darwin %>% 
+  pivot_wider(names_from = type, values_from = height) %>% 
+  mutate(difference = Cross - Self)
+
+difference_summary %>% 
+  mutate(se= sd/sqrt(n))
+
+#Create a sequence of 100 equally spaced numbers between -4 and 4
+x <- seq(-4, 4, length=100)
+
+#create a vector of values that shows the height of the probability distribution
+#for each value in x
+y <- dnorm(x)
+
+#plot x and y as a scatterplot with connected lines (type = "l") and add
+#an x-axis with custom labels
+plot(x,y, type = "l", lwd = 2, axes = FALSE, xlab = "", ylab = "")
+axis(1, at = -3:3, labels = c("-3s", "-2s", "-1s", "mean", "1s", "2s", "3s"))
+
+#confident intrvals 
+lowerCI <- 2.62-(2*1.22)
+
+upperCI <- 2.62+(2*1.22)
+
+lowerCI
+upperCI
+
