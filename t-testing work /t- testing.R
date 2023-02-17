7f2b1808e4983e5d3a6cbf34cc3b877bfadcc62a
decariedavies <decarie.davies@uea.ac.uk>
2023-02-17 13:31:05 +0000

    creating multiple graphs that show confident intervals and p values via multiple testing

---
+++ t-testing work /t- testing.R
@@ -0,0 +1,57 @@
+# t test helps us identify normal distribution alongside other graphs that help us to estimate the mean difference of confidence intervals
+# the base r and te tidyverse ran will create different structured graphs trhe difference is ..... (write when im home what the difernece is)
+
+##base r code---- 
+x <- seq(-4, 4, length=100)
+hx <- dnorm(x)
+
+degf <- c(1, 3, 8, 30)
+colors <- c("red", "blue", "darkgreen", "gold", "black")
+labels <- c("df=1", "df=3", "df=8", "df=30", "normal")
+
+plot(x, hx, type="l", lty=2, xlab="x value",
+     ylab="Density", main="Comparison of t Distributions")
+
+for (i in 1:4){
+  lines(x, dt(x,degf[i]), lwd=2, col=colors[i])
+}
+
+legend("topright", inset=.05, title="Distributions",
+       labels, lwd=2, lty=c(1, 1, 1, 1, 2), col=colors)
+
+##Tidyverse code----
+
+
+# map_dfc combines values returned into a data frame
+x <- seq(-4, 4, length=100)
+z_dist <- dnorm(x)
+
+values <- tibble(x,z_dist)
+
+t <- map_dfc(degf, ~dt(x, .x))
+colnames(t) <- degf
+
+combined <- cbind(values,t)
+
+combined %>% 
+  pivot_longer(cols=!x, names_to="distribution") %>% 
+  mutate(distribution=factor(distribution, levels=c("z_dist", "1", "3", "8", "30"))) %>%  
+  mutate(distribution=fct_recode(distribution, "z distribution" = "z_dist", "df = 1" = "1", "df = 3" = "3", "df = 8" = "8", "df = 30" = "30")) %>% 
+  ggplot(aes(x=x, y=value, colour=distribution))+
+  geom_line(linetype="dashed")+
+  theme_classic()
+
+#we do this to analyse the generated value by the inear models and the outputs created
+df <- c(1:30)
+
+# map_dbl forces returned values to be a single vector of numbers (rather than a list)
+critical_t <- map_dbl(df, ~qt(p=0.05/2, df=.x, lower.tail=FALSE))
+
+tibble(df,critical_t) %>% 
+  ggplot(aes(x=df, y=critical_t))+
+  geom_point()+
+  geom_line()+
+  geom_hline(aes(yintercept=1.96), linetype="dashed", colour="red")+
+  labs(x= "Degrees of Freedom",
+       y= expression(paste("Critical value of ", italic("t"))))
+base r
